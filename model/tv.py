7e35042cdb675f7420b5eb4c007f82fdfdd147c3
BeiYuu <dewreg@gmail.com>
2014-01-08 00:45:52 +0800

    edit item

--- model/tv.py
+++ model/tv.py
@@ -6,37 +6,68 @@ class TV():
     def __init__(self, id):
         tv = mongo.db.tv_show.find_one({'_id':ObjectId(id)})
         self.id = id
-        self.name= tv['name']
-        self.cname= tv['cname']
+        self.url_name = tv['url_name']
+        self.cn_name = tv['cn_name']
+        self.origin_name = tv['origin_name']
+        self.aka_name = tv['aka_name']
+        self.directors = tv['directors']
+        self.casts = tv['casts']
+        self.writers = tv['writers']
+        self.languages = tv['languages']
+        self.countries = tv['countries']
+        self.summary = tv['summary']
+        self.is_ended = tv['is_ended']
 
     @property
-    def name(self):
-        """Get the tv cnname."""
-        return self.nname
+    def cn_name(self):
+        return self.cn_name
 
     @property
-    def cname(self):
-        """Get the tv cnname."""
-        return self.cnname
+    def url_name(self):
+        return self.url_name
+
+    @property
+    def origin_name(self):
+        return self.origin_name
+
+    @property
+    def aka_name(self):
+        return self.aka_name
+
+    @property
+    def directors(self):
+        return self.directors
+
+    @property
+    def casts(self):
+        return self.casts
+
+    @property
+    def writers(self):
+        return self.writers
+
+    @property
+    def languages(self):
+        return self.languages
+
+    @property
+    def countries(self):
+        return self.countries
+
+    @property
+    def summary(self):
+        return self.summary
+
+    @property
+    def is_ended(self):
+        return self.is_ended
 
     @classmethod
     def get(cls, url_name):
         if not url_name:
             return None
-        tv = mongo.db.tv_show.find_one({'$or': [{'url_name': url_name}]})
+        tv = mongo.db.tv_show.find_one({'url_name': url_name})
         return cls(tv['_id']) if tv else None
-
-    def getAllEpi(self):
-        pass
-
-    def getEpi(self):
-        pass
-
-    def editEpi(self):
-        pass
-
-    def addEpi(self):
-        pass
 
     @classmethod
     def addTV(cls, cn_name, url_name, origin_name, aka_name, directors,
@@ -47,6 +78,7 @@ class TV():
             'url_name': url_name,
             'origin_name': origin_name,
             'aka_name': aka_name,
+            'directors': directors,
             'casts': casts,
             'writers': writers,
             'languages': languages,
@@ -54,3 +86,24 @@ class TV():
             'summary': summary,
             'is_ended': is_ended
         })
+
+    @classmethod
+    def updateTV(cls, cn_name, url_name, origin_name, aka_name, directors,
+            casts, writers, languages, countries, summary, is_ended):
+
+        mongo.db.tv_show.update(
+                {'url_name': url_name},
+                {'$set': {
+                    'cn_name': cn_name,
+                    'origin_name': origin_name,
+                    'aka_name': aka_name,
+                    'directors': directors,
+                    'casts': casts,
+                    'writers': writers,
+                    'languages': languages,
+                    'countries': countries,
+                    'summary': summary,
+                    'is_ended': is_ended
+                    }
+                }
+        )
--- view/add_edit.py
+++ view/add_edit.py
@@ -4,25 +4,26 @@ from homeland import app
 from homeland.model.tv import TV
 from flask.ext.mako import render_template
 
+def list_item(form, item_name):
+    ret = form.getlist(item_name)
+    ret = [x.strip() for x in ret]
+    ret = [x for x in ret if x]
+    return ret
+
 @app.route('/add/', methods=['GET', 'POST'])
 def add_tv():
+    edit_tv = False
     if request.method == 'POST':
-        def list_item(item_name):
-            ret = form.getlist(item_name)
-            ret = [x.strip() for x in ret]
-            ret = [x for x in ret if x]
-            return ret
-
         form = request.form
         cn_name = form.get('cn_name').strip()
         url_name = form.get('url_name').strip()
         origin_name = form.get('origin_name').strip()
-        aka_name = list_item('aka_name')
-        directors = list_item('directors')
-        casts = list_item('casts')
-        writers = list_item('writers')
-        languages = list_item('languages')
-        countries = list_item('countries')
+        aka_name = list_item(form, 'aka_name')
+        directors = list_item(form, 'directors')
+        casts = list_item(form, 'casts')
+        writers = list_item(form, 'writers')
+        languages = list_item(form, 'languages')
+        countries = list_item(form, 'countries')
         summary = form.get('summary').strip()
         is_ended = int(form.get('is_ended', 0))
 
@@ -42,6 +43,48 @@ def add_tv():
 
     return render_template('add_edit.html', **locals())
 
-@app.route('/edit/<url_name>')
-def edit_tv(url_nme):
-    return render_template('add_edit.html')
+@app.route('/<url_name>/edit/', methods=['GET', 'POST'])
+def edit_tv(url_name):
+    tv = TV.get(url_name)
+    edit_tv = True
+    eidt_succ = False
+
+    if request.method == 'POST':
+        form = request.form
+        cn_name = form.get('cn_name').strip()
+        origin_name = form.get('origin_name').strip()
+        aka_name = list_item(form, 'aka_name')
+        directors = list_item(form, 'directors')
+        casts = list_item(form, 'casts')
+        writers = list_item(form, 'writers')
+        languages = list_item(form, 'languages')
+        countries = list_item(form, 'countries')
+        summary = form.get('summary').strip()
+        is_ended = int(form.get('is_ended', 0))
+
+        if not cn_name:
+            error = '请填写剧集中文名称'
+            return render_template('add_edit.html', **locals())
+
+        TV.updateTV(cn_name=cn_name, url_name=url_name, origin_name=origin_name,
+                aka_name=aka_name, directors=directors, casts=casts, writers=writers,
+                languages=languages, countries=countries, summary=summary,
+                is_ended=is_ended)
+        edit_succ = True
+        return render_template('add_edit.html', **locals())
+
+    if tv:
+        cn_name = tv.cn_name
+        url_name = tv.url_name
+        origin_name = tv.origin_name
+        aka_name = tv.aka_name
+        directors = tv.directors
+        casts = tv.casts
+        writers = tv.writers
+        languages = tv.languages
+        countries = tv.countries
+        summary = tv.summary
+        is_ended = tv.is_ended
+        return render_template('add_edit.html', **locals())
+    else:
+        abort(404)
